122837e149c596edfdd8020cb5d62d5c63959da5
shahbazalam8 <shahbazalam8@github.com>
2021-10-10 19:50:24 +0530

    adding runner main.tf and vars file

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_s3_bucket_object" "clust_name" {
+  bucket = "trainingusbatch"
+  key    = "myapp/staging/current"
+}
+
+module "eks" {
+  #source  = "github.com/synectiks-sddc/terraform-aws-customEKS.git"
+  source   = "/mnt/d/mycode/terraform-aws-customEKS" #"/opt/mycode/terraform-aws-eks"
+  #version = "0.0.1"
+  # insert the 1 required variable here
+  #cluster_version = "1.20"
+  cluster_name    = "${data.aws_s3_bucket_object.clust_name.body}" #var.cluster_name
+  cluster_version = var.cluster_version
+  #region 	  = var.region
+}
